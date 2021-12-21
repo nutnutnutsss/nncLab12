6254bb301e2fb88bc505d3c64ef54ad4e942cfa0
NutNatcha <natcha_me@cmu.ac.th>
2021-12-21 16:33:03 +0700

    lab12_1 complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,27 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i = 0; i < 3; i++){
+        cin.get();
+    }
+    int x = rand()%9;
+    string grade;
+    if(x == 0) grade = "A";
+    else if(x == 1) grade = "B+";
+    else if(x == 2) grade = "B";
+    else if(x == 3) grade = "C+";
+    else if(x == 4) grade = "C";
+    else if(x == 5) grade = "D+";
+    else if(x == 6) grade = "D";
+    else if(x == 7) grade = "F";
+    else if(x == 8) grade = "W";
+
+    cout << "You will get " << grade << " in this 261102.";
+}
+
